1799e7b885158b5cf7ff21a7a63d35347962d979
Taha Shabbir <139618419+taha12-ok@users.noreply.github.com>
2024-09-10 01:04:01 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
 import RandomJoke from "@/components/ui/random-joke";
 export default function Home() {
   return (
